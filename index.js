4c4d83aa988beb6ab5e31d7bb9d9040abb7136ad
leort11 <leonardo.figueiredo31@hotmail.com>
2025-06-09 17:10:33 -0300

    chore: initialize package.json with express dependency

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, (error) => {
+    if(error) {
+        console.log("Deu erro: ");
+        return;
+    }
+    console.log("Subiu na porta " + port);
+});
